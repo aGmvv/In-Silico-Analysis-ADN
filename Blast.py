f8263141e668ac1b0c3680e0370f2d7a35578d0f
aGmvv <116973654+aGmvv@users.noreply.github.com>
2023-03-16 14:49:36 +0100

    Update Blast.py

--- Blast.py
+++ Blast.py
@@ -32,25 +32,3 @@ def blast(primers, patogeno, evalue, maxseqs, extrar, extrap):
     print("FINISH BLAST")
 
 
-def blast_prueba(primers, patogeno, evalue, maxseqs, extrar, extrap):
-    """
-
-    :param primers: Contiene los primers que queremos buscar en blast.
-    :param patogeno: El nombre del archivo que se va a generar.
-    :return:
-    """
-
-    # os.chdir('./bin')  # Cambiar al directorio ./bin, donde se va a generar el archivo.
-    e_value = " -evalue " + evalue
-    init = "blastn -task blastn-short "
-    db = "-db nt "
-    max_seqs = " -max_target_seqs " + maxseqs
-    out = "-out " + patogeno
-    extra = " -reward " + extrar + " -penalty " + extrap
-    form = " -outfmt " + '"6 qseqid sacc sstart send qstart qend mismatch sskingdoms sblastnames sscinames ' \
-                         'sstrand evalue qseq sseq" '
-
-    totalquery = init + db + "-query " + str(primers) + e_value + ' -remote ' + out + max_seqs + extra + form
-
-    return totalquery
-
